19e19fe67989c3cd5b3c7008d15e917e565891c6
Matthijs Raatgever <matthijs4004@gmail.com>
2021-09-06 13:29:43 +0200

    python reken programma

---
+++ feestlunch.py
@@ -0,0 +1,6 @@
+Croissantjes = 17
+PrijsCroissant = 0.39
+Stokbroden = 2
+PrijsStokbrood = 2.78
+Korting = 1.50
+print((Croissantjes * PrijsCroissant) + (Stokbroden * PrijsStokbrood) - Korting)
---
+++ speelhaldag.py
@@ -0,0 +1,4 @@
+ToegangsPrijs = 7.45
+PrijsPer5min = 0.37
+
+print((ToegangsPrijs * 4) + (PrijsPer5min * 9 ))
