0765b094cde5b6d9ade14d60a8a3b1bf30762646
Matthijs Raatgever <matthijs4004@gmail.com>
2021-09-06 15:12:44 +0200

    nette output

--- feestlunch.py
+++ feestlunch.py
@@ -3,4 +3,9 @@ PrijsCroissant = 0.39
 Stokbroden = 2
 PrijsStokbrood = 2.78
 Korting = 1.50
-print((Croissantjes * PrijsCroissant) + (Stokbroden * PrijsStokbrood) - Korting)
+
+bedrag = ((Croissantjes * PrijsCroissant) + (Stokbroden * PrijsStokbrood) - Korting)
+
+factuurtekst = "De feestlunch kost je bij de bakker " + str(bedrag) + " euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!"
+
+print(factuurtekst)
--- speelhaldag.py
+++ speelhaldag.py
@@ -1,4 +1,8 @@
 ToegangsPrijs = 7.45
 PrijsPer5min = 0.37
 
-print((ToegangsPrijs * 4) + (PrijsPer5min * 9 ))
+bedrag = ((ToegangsPrijs * 4) + (PrijsPer5min * 9 ))
+
+factuurtekst = "Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar " + str(bedrag) + " euro "
+
+print(factuurtekst)
